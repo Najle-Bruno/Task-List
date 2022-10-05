f95720bada34daaf5fde6340d06cd2b697f37953
Bruno Najle <brussxwx@gmail.com>
2022-10-05 15:58:16 -0300

    Tasks-items

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from '../Task';
 import { TASK } from '../mock-tasks';
-
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-task',
@@ -10,6 +10,7 @@ import { TASK } from '../mock-tasks';
 })
 export class TaskComponent implements OnInit {
  task: Task[] = TASK;
+ faTimes = faTimes
   constructor() { }
 
   ngOnInit(): void {
--- src/app/components/tasks-item/tasks-item.component.ts
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Task } from '../Task';
 import { TASK } from '../mock-tasks';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-tasks-item',
@@ -9,6 +11,8 @@ import { TASK } from '../mock-tasks';
 })
 export class TasksItemComponent implements OnInit {
 @Input() task: Task = TASK [0]
+faTimes = faTimes
+
   constructor() { }
 
   ngOnInit(): void {
